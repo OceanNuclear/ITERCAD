41979ddf378311458a9854a637be75e5c9c9f7e9
OceanNuclear <annoyingamygdala@gmail.com>
2020-05-21 21:52:23 +0100

    finished simulation

--- particlerelax.py
+++ particlerelax.py
@@ -2,16 +2,12 @@ import numpy as np
 from numpy import array as ary
 from numpy import sqrt, tan, pi, sin, cos
 from math import fsum
+import os
 from SimpleMapping import *
 #import seaborn as sns
 from scipy.stats import describe
 from time import time
 import logging
-logger = logging.getLogger(__name__) #created logger with NOSET level of logging, i.e. all messages are recorded.
-# logFormatter = '%(asctime)s - %(levelname)s - %(message)s'
-logger.setLevel(logging.DEBUG)
-logHandler = logging.FileHandler('first_run.log')
-handler = logger.addHandler(logHandler)
 
 #given array of points as all_points
 RAD = 0.15
@@ -22,13 +18,32 @@ RESOLUTION = 1200
 area_sextant = pi*(radius_max**2 - radius_min**2)/6
 wire_radius = sqrt(area_sextant/417)/2 # this is only an approximation, assuming square packing.
 F_LIMIT = wire_radius * 0.5E-2 # 
+NEW_KERNEL = True
 
-def kernel(scaled_dist):
-	scale_fac = pi/2 #scale to the right radius
-	effective_distance = np.clip(scaled_dist*scale_fac, 0, 1)
-	return 1/scale_fac * 1/tan(scaled_dist*scale_fac) # cot(number/(pi/2) ), the 1/scale_fac is intended to keep the slope =-1.
-def inv_kernel(scaled_dist):
-	return pi/2*tan(pi/2*scaled_dist)
+RUNNING_ON_LOCAL = 'ocean' in os.uname()[1]
+if RUNNING_ON_LOCAL:
+	import matplotlib.pyplot as plt
+
+if NEW_KERNEL:
+	def kernel(scaled_dist):
+		return 1- scaled_dist
+
+	PARABOLIC_PART_WEIGHT = 0.5
+	def kernel_2(scaled_dist):
+		return 1- scaled_dist + PARABOLIC_PART_WEIGHT*(1- scaled_dist)**2
+		
+	def inv_kernel(scaled_dist):
+		return 1-scaled_dist
+
+	def inv_kernel_2(y):
+		return ( (1/PARABOLIC_PART_WEIGHT +2) - sqrt( (1/PARABOLIC_PART_WEIGHT + 2)**2 - 4*(1/PARABOLIC_PART_WEIGHT+1-y) ) )/2
+else:
+	def kernel(scaled_dist):
+		scale_fac = pi/2 #scale to the right radius
+		return 1/scale_fac * 1/tan(scaled_dist*scale_fac) # cot(number/(pi/2) ), the 1/scale_fac is intended to keep the slope =-1.
+
+	def inv_kernel(y): # This function seems wrong...
+		return pi/2*tan(pi/2*y)
 
 class Point:
 	def __init__(self, pos, pk=kernel, wk=kernel):
@@ -47,10 +62,26 @@ class Point:
 		scaled_dist = dist/self.wall_effect_thickness
 		return self.wall_effect_thickness * self.wk_raw(scaled_dist) #again, these are meant to keep its slope = -1
 		
+	def get_force_soph(self, all_points, i):
+		force = []
+		
+		mask = np.ones(len(all_points), dtype=bool)
+		mask[i] = False
+		for p in ary(all_points)[mask]: # have to convert it into an array in order to work with mask methods.
+			displacement = self.pos - p.pos
+			dist = quadrature(displacement)
+
+			if dist < self.radius_of_effectiveness:
+				direc = displacement/dist
+				mag = self.point_kernel(dist)
+				force.append(mag*direc)
+		return ary(force).reshape([-1,2])
+
 	def get_force_raw(self, all_points):
-		#exclude itself
+		force = []
 		wall_force = self.wall_repel()
-		force = ary([wall_force,])
+		if quadrature(wall_force): # if the force is non-zero:
+			force.append(wall_force)
 		
 		for p in all_points:
 			if not all(p.pos== self.pos): # for all points that isn't itself:
@@ -60,10 +91,8 @@ class Point:
 				if dist < self.radius_of_effectiveness: # if the point is close enough:
 					direc = displacement/dist # normalize the negative displacement vector
 					mag = self.point_kernel(dist)
-					force = np.append(force, [mag*direc], axis=0)
-		# if not all(wall_force==[0,0]):
-			# force = np.append(force, [self.wall_repel()], axis=0 )
-		return force
+					force.append(mag*direc)
+		return ary(force).reshape([-1,2]) #ary([[x,y],[x,y],...])
 
 	def get_force(self, all_points):
 		force = self.get_force_raw(all_points)
@@ -105,6 +134,12 @@ def easy_logspace(start, stop, num):
 	return np.logspace(np.log10(start), np.log10(stop), num)
 
 if __name__=="__main__":
+	logger = logging.getLogger(__name__) #created logger with NOSET level of logging, i.e. all messages are recorded.
+	# logFormatter = '%(asctime)s - %(levelname)s - %(message)s'
+	logger.setLevel(logging.DEBUG)
+	logHandler = logging.FileHandler('first_run.log')
+	handler = logger.addHandler(logHandler)
+
 	circle = tessellate_circle_properly(417)
 	logger.info("Using reference step size={0}, RAD = {1}".format(REF_STEP_SIZE, RAD))
 	cable = [] #sorted by [layer=theta][strand_number][xy_coords]
@@ -138,11 +173,17 @@ if __name__=="__main__":
 			
 			#calculate step size to take
 			if des.minmax[1] < F_LIMIT*15 and des.mean < F_LIMIT*15/2.5:
-				step_size = 1.8*REF_STEP_SIZE
+				if NEW_KERNEL:
+					step_size=0.9
+				else:
+					step_size = 1.8*REF_STEP_SIZE
 			else:
-				x = ary([ force_mag[i]/all_points[i].radius_of_effectiveness for i in range(len(force_mag)) ])
-				dev_factor = (x)*inv_kernel(1-x) #make sure that it's not deviated by too much.
-				step_size = REF_STEP_SIZE*min(dev_factor)
+				if NEW_KERNEL:
+					step_size=1
+				else:
+					x = ary([ force_mag[i]/all_points[i].radius_of_effectiveness for i in range(len(force_mag)) ])
+					dev_factor = (x)*inv_kernel(1-x) #make sure that it's not deviated by too much.
+					step_size = REF_STEP_SIZE*min(dev_factor)
 
 			#print these information for debug purposes.
 			logger.debug("Step {0} has a mean force of {1} with bounds of {2} and skewness={3}. Taking a step size of {4}".format( step, des.mean, des.minmax, des.skewness, step_size) )
@@ -153,14 +194,15 @@ if __name__=="__main__":
 		
 		relaxed_pos = ary([p.pos for p in all_points])
 		cable.append(relaxed_pos)
-		with open ("single_cable_data.txt", "a+") as f:
-			f.write(np.array2string(relaxed_pos, separator=','))
-			f.write(",\n")
 		logger.info("Finished angle {0} using {1} steps at t={2}s after the start time".format(theta, step, time()-start_t) )
-		'''
-		#plotting for checking for anomalies.
-		plt.scatter(relaxed_pos[:,0], relaxed_pos[:,1] )
-		plt.title("Final distribution after relaxing for {} steps".format(NUM_STEPS))
-		plt.show()
-		'''
-	np.save('single_cable_data.npy', cable)
+		if RUNNING_ON_LOCAL:
+			#plotting for checking for anomalies.
+			plt.scatter(relaxed_pos[:,0], relaxed_pos[:,1] )
+			plt.title("Final distribution after relaxing for {} steps".format(NUM_STEPS))
+			plt.show()
+		else:
+			with open ("single_cable_data.txt", "a+") as f:
+				f.write(np.array2string(relaxed_pos, separator=','))
+				f.write(",\n")
+	if not RUNNING_ON_LOCAL:
+		np.save('single_cable_data.npy', cable)
--- relaxvideo.py
+++ relaxvideo.py
@@ -6,8 +6,9 @@ from ConvrgenceVideo import get_outline
 from create_spiral import get_closest_neighbour_distance
 from scipy.stats import describe
 
-VIDEO = False
+VIDEO = True
 READ_FROM_HUMAN_READABLE_FILE= False
+EVERY_NTH_FRAME=1
 
 target_r_min_to_r_max_ratio = 0.45/1.985 #both radii measurements were recorded in mm.
 offset_constant_in_sqrt = 12 * (1 + target_r_min_to_r_max_ratio**2)/(1 - target_r_min_to_r_max_ratio**2)
@@ -25,6 +26,16 @@ def str2array(l):
         else: 
             text[p] += i 
     return ary([np.fromstring(entry.lstrip(', '), sep=',') for entry in text])
+
+# def reorder_points(slice_below, slice_here):
+#     new_order_points = []
+#     for i in range(slice_below.num_points):
+#         p = slice_below.points[i]
+#         .append([ quadrature(disp) for disp in ary(slice_here) - p.pos])
+
+#         new_order_points.append(slice_here.points[j])
+#     slice_here.points = new_order_points # need to check if there are duplicates though
+#     return slice_here
 
 def check_in_bound(p):
     dist_from_centre = quadrature(p)
@@ -44,12 +55,15 @@ if __name__=='__main__':
             frame_data.append(str2array(data[i][1:-1]))
     else:
         from interframeattract import *
-        column = np.load('repel_result.npy', allow_pickle=True)
+        column = np.load('submit_jobs/run4/repel_result_stronger.npy', allow_pickle=True)
+        # coordinates = []
+        # for s in column:
+        #     coordinates.append([p.pos for p in in s.points])
         for i in range(len(column)):
             frame_data.append( [column[i].points[j].pos for j in range(column[i].num_points)] )
 
     FFMpegWriter = manimation.writers['ffmpeg']
-    metadata = dict(title='short_repel', artist='Matplotlib', comment='100 frames, Triple repel')
+    metadata = dict(title='linear_result', artist='Matplotlib', comment='100 frames, Triple repel')
     writer = FFMpegWriter(fps=15, metadata=metadata)
     fig, ax = plt.subplots()
     ax.set_aspect(1.0)
@@ -59,19 +73,25 @@ if __name__=='__main__':
     ax.set_yticks([])
     outline = ary(get_outline()).T
     ax.plot(outline[0], outline[1])
-    xycoods = ax.scatter(np.ones(417), np.zeros(417))
+    xycoods1 = ax.scatter(np.ones(417)[::3], np.zeros(417)[::3])
+    xycoods2 = ax.scatter(np.ones(417)[1::3], np.zeros(417)[1::3])
+    xycoods3 = ax.scatter(np.ones(417)[2::3], np.zeros(417)[2::3])
                
     if VIDEO:
-        with writer.saving(fig, "short_repel.mp4", 300):
+        with writer.saving(fig, "linear_result_stronger.mp4", 300):
             for i in range(len(frame_data)):
-                xycoods.set_offsets( frame_data[i] )
-                ax.set_title("Layer "+str(i))
-                writer.grab_frame()
+                if i%EVERY_NTH_FRAME==0:
+                    xycoods1.set_offsets( frame_data[i][::3] )
+                    xycoods2.set_offsets( frame_data[i][1::3] )
+                    xycoods3.set_offsets( frame_data[i][2::3] )
+                    ax.set_title("Layer "+str(i))
+                    writer.grab_frame()
             fig.clf()
     else:
         plt.cla()
         for i in range(len(frame_data)):
             distances = get_closest_neighbour_distance(frame_data[i])
+            # frame_data[i+1], frame_data[i-1]
             des = describe(distances)
             print(des)
             if sum(n:=[ check_in_bound(p) for p in frame_data[i] ])>0:
