83338a10e099a4eed16323fe8d631154e189adc5
OceanNuclear <annoyingamygdala@gmail.com>
2020-05-31 15:26:23 +0100

    setting 8

--- repel_attract.py
+++ repel_attract.py
@@ -232,12 +232,11 @@ if __name__=='__main__':
             sum_vel, max_vecs = lay_out_vel_vecs(forces)
             weights.append(max_vecs)
             final_velocity.append(sum_vel)
-        for this_wall_vec in wall_vel_vecs:
-            weights.append(1)
-            final_velocity.append(lay_out_wall_vel_vecs(this_wall_vec))
-
-        for vel, max_vecs in zip(final_velocity, weights):
+        for ind, (vel, max_vecs) in enumerate(zip(final_velocity, weights)):
             real_data += vel * max_vecs/sum(weights) * 0.4
+
+        for this_wall_vec in wall_vel_vecs:
+            real_data += lay_out_wall_vel_vecs(this_wall_vec)/2 * 0.4 #amplify the forces from the walls back to 1 instead of 0.25
         # The setting (0.3, 0.1 worked for 26 steps, but then started diverging soon after)
         
         real_data += (upper_attract+lower_attract)/2 * 0.05
@@ -246,7 +245,7 @@ if __name__=='__main__':
 
         print('Taken step={} at time={}'.format(num_steps, time.time()-starttime))
         np.save('repel_attract.npy', real_data)
-        
+
         # # printing information:
         # print(describe(lower_attract[0]))
         # print(describe(upper_attract[0]))
